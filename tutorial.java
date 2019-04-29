31671aac6a5a39450adf94cc903944a41a714225
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-29 18:11:27 +0000

    java basic

---
+++ tutorial.java
@@ -0,0 +1,16 @@
+class Tutorial {
+     public static void main (String[] args){ 
+         
+         int a = 23;
+         byte b; // 8 bits of memory 
+         short s;// more than a bit 16 bits
+         long l; // 64 bits 
+         float f; //32 bits 
+         double d // 64 bits 
+         
+         
+         System.out.println("Hello World");
+         System.out.println(a);
+     }
+    
+}

989b3d2a72ff6f1d4e348fe0bbe0551542bd80ef
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-29 18:43:07 +0000

    java numbers and string

--- tutorial.java
+++ tutorial.java
@@ -4,13 +4,25 @@ class Tutorial {
          int a = 23;
          byte b; // 8 bits of memory 
          short s;// more than a bit 16 bits
-         long l; // 64 bits 
-         float f; //32 bits 
-         double d // 64 bits 
+         long l; // 64 bits  100000000L
+         float f; //32 bits 3.14f
+         double d // 64 bits 314d
          
          
          System.out.println("Hello World");
          System.out.println(a);
+     }
+     
+     public class Tutorial {
+         public static void main(String{} , args){
+             
+             int a = 10; 
+             int b = 300; 
+             int c = a + b;
+             
+             String helloWorld = "Hello World"
+             System.out.printlin(helloWorld)
+         }
      }
     
 }

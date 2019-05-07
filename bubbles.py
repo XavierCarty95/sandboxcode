feff53e07b10f078027cc860e5d0b214bfc9fdea
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-05-07 15:15:20 +0000

    computational thinking

--- bubbles.py
+++ bubbles.py
@@ -1,6 +1,7 @@
 # Get the highest scores in the table
 # Get the length of the table
 # Solutions to get the most cost effective price from the array table
+
 scores = [ 60 , 50 , 60 , 58 , 54 ,54 , 50 , 52, 54 ,48,
             69,34,55,51,52,44,51,69,64,66,55,52,61,46,31,
             57,52,44,18,41,53,55,61,51,44]

de0fe3ab2a45fe86d8013b9511132c8790f41fc6
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-05-08 01:38:53 +0000

    functions , else-if

---
+++ travel.py
@@ -0,0 +1,9 @@
+def how_should_i_get_there(miles , kilometer):
+    
+    if miles > 120.0: 
+        print('Take a plane')
+    elif miles >= 2.0:
+        print("Take a car")
+    else:
+        print('Walk')
+        

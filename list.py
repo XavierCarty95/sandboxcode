97e9939f012ca3c4ca665d04e3b6b2c88d79a3a8
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-05-10 17:28:53 +0000

    list in python

---
+++ list.py
@@ -0,0 +1,42 @@
+ruit = list()
+print(fruit)
+
+fruit = ["Apple", "Orange", "Pear"]
+print(fruit)
+
+fruit.append("Banana")
+fruit.append("Peach")
+print(fruit)
+
+random = []
+random.append(True)
+random.append(100)
+random.append(1.1)
+random.append("Hello")
+print(random)
+
+fruit[0]
+fruit[1]
+fruit[2]
+
+
+fruit = ["Apple", "Orange", "Pear"]
+length = len(fruit)
+
+colors = ["blue" , "green", "yellow"]
+colors 
+colors[2] = "red"
+print(colors)
+
+colors = ["blue" , "green", "yellow"]
+items = colors.pop()
+print(colors,items)
+
+colors = ["blue" , "green", "yellow"]
+colors1 = ["blue" , "green", "yellow"]
+print(colors + colors1)
+
+colors = ["blue" , "green", "yellow"]
+"green" in colors 
+
+print(len(colors))
---
+++ methods.py
@@ -0,0 +1,5 @@
+fruit = ["Apple", "Orange", "Pear"]
+
+for i in fruit: 
+    fruit[i].replace("O","@")
+print(fruit)

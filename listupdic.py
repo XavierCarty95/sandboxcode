e766cb6c5a65936254d6d236b751890fd00034c3
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-05-10 18:45:05 +0000

    adding list and tuples in dictionaries

---
+++ listupdic.py
@@ -0,0 +1,24 @@
+location = []
+la = (34.0522 , 188.2437)
+chicago = (41.8781, 87.6298)
+
+
+
+location.append(la)
+location.append(chicago)
+
+
+print(location)
+
+eights = ["Edgar Allen Poe" , "Charles Dickens"]
+
+nines = ["Hemingway" , "Fitzgerald" , "Orwell"]
+
+authors = (eights,nines)
+print(authors)
+
+bday = {"Hemingway":"7.21.1899", "Fritzgerald":"9.24.1896"}
+my_list = [bday]
+print(my_list)
+my_tuple = (bday,)
+print(my_tuple)

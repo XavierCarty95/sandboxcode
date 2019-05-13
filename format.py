4e1eb6f703ade8b8b86fda22bcf049dc046f72c8
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-05-13 16:27:25 +0000

    format string manipulation

---
+++ format.py
@@ -0,0 +1,8 @@
+n1 = input("Enter a noun: ")
+v = input("Enter a verb: ")
+adj = input("Enter a adj: ")
+n2 = input ("Enter a noun:")
+
+r = """ The {}{} the {} {} """.format(n1,v,adj,n2)
+
+print(r)

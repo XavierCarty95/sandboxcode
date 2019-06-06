01b5217e5069b980762afbf7a3660d1742f02f23
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-06-06 22:51:06 +0000

    rock paper scissors upgraded

--- rock.py
+++ rock.py
@@ -1,43 +1,43 @@
-import random 
+import random
 
-winner = ' '
+winner = ''
 
 random_choice = random.randint(0,2)
 
-if random_choice == 0: 
+if random_choice == 0:
     computer_choice = 'rock'
-elif random_choice == 1: 
+
+elif random_choice == 1:
     computer_choice = 'paper'
 
 else:
     computer_choice = 'scissors'
 
-user_choice = input('rock, paper, or scissors?')
 
-if(user_choice != 'rock' and 
-     user_choice != 'paper' and 
-     user_choice != 'scissors'):
-    user_choice = input('rock , paper or scissors?')
-print('User chose' , user_choice)
+user_choice = ''
+while(user_choice != 'rock' and
+      user_choice != 'scissors'
+      and user_choice != 'paper'):
+   user_choice = input('rock, paper, or scissors?')
+
+if computer_choice == 'paper' and user_choice == 'rock':
+    winner = "Computer"
+
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    winner = "Computer"
+
+elif computer_choice == 'rock' and user_choice == 'scissor':
+    winner = "Computer"
+
+else:
+    winner = 'User'
 
 if computer_choice == user_choice:
     winner = 'Tie'
 
-elif computer_choice == 'paper' and user_choice == 'rock':
-    winner = 'Computer'
-
-elif computer_choice == 'rock' and user_choice == 'scissors':
-    winner = 'User'
-elif computer_choice == 'scissors' and user_choice == 'paper':
-    winner = 'Computer'
-
-
-    winner = 'User'
-
-print('The', winner, 'wins!')
-
 if winner == 'Tie':
-    print('We both chose', computer_choice + ',play again')
-    
-else: 
-    print(winner, 'won. The computer chose', computer_choice + '.')
+    print('We both chose', computer_choice + ', play again')
+
+else:
+    print(winner, 'won. The computer chose' , computer_choice + '.')
+
